bd7ff801a0549809682772a3e7ff4008b764aa07
testing-gems <shailesh.butter@gmail.com>
2024-09-11 19:57:53 +0530

    Initial commit

---
+++ src/screens/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <div>
+         Body
+        </div>
+        <div>
+          <Footer/>
+        </div>
+    </div>
+  )
+}
